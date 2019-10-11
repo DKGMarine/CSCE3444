b32c1e07ff5cab97967ba7e94a53e176031aa57d
bm0452 <bm0452@my.unt.edu>
2019-10-10 20:21:09 -0500

    Updating Files

---
+++ app/src/main/java/com/example/movierater/Movie.java
@@ -0,0 +1,19 @@
+package com.example.movierater;
+
+import java.io.Serializable;
+
+public class Movie implements Serializable {
+
+
+     public String title;
+     public int duration; // minutes
+     public int release_year;
+     public boolean netflix;
+     public boolean hulu;
+     public boolean prime_video;
+     public boolean sling_tv;
+     public boolean hbo_now;
+     public boolean youtube_tv;
+     public boolean google_play;
+
+}
